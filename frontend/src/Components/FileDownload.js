54e0be3ab7e7f5a9ea856cf3c0ee2f96df829d3f
Programmer5768964 <svishwakarma6389@gmail.com>
2024-03-05 17:23:50 +0530

    pentart-client-server

---
+++ frontend/src/Components/FileDownload.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Axios from 'axios';
+
+import fileDownload from 'js-file-download';
+
+const DownloadControle = ()=>{
+
+
+
+const Download = ()=>{
+    Axios({
+        url:"http://localhost:5000/download",
+        method:"GET",
+        responseType:"blob"
+    }).then((resp)=>{
+        console.log(resp)
+        fileDownload(resp.data,"downloaded.png")
+    })
+}
+
+return (
+    <div>
+        <button onClick={(e)=>{Download(e)}} type='button'>Download</button>
+    </div>
+
+
+)
+
+}
+
+export default DownloadControle;
---
+++ frontend/src/pages/Potrait.jsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import fileDownload from "js-file-download";
+
+import "./listpotrait.css";
+import { BiSearch } from "react-icons/bi";
+
+// import { isHtmlElement } from 'react-router-dom/dist/dom';
+const ShowPotraits = () => {
+  const [data, setData] = useState([]);
+  // const navigate = useNavigate();
+  // let [name, setName] = useState('');
+  useEffect(() => {
+    getPotrait();
+  }, []);
+
+  const getPotrait = () => {
+    axios
+      .get("http://localhost:5000/potrait")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err, "error has occured"));
+  };
+  // const numbers = [1, 2, 3, 4, 5];
+  // let count = 0;
+
+  const searchPotrait = async (key) => {
+    console.log("my key is", key);
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setData(result);
+      }
+    } else {
+      getPotrait();
+    }
+  };
+  // const DownloadImg = async (Value) => {
+  //     let key = Value;
+
+  //     let result = await fetch("http://localhost:5000/download", {
+  //         method: "post",
+  //         responseType: "blob",
+  //         body: JSON.stringify({ name })
+  //     })
+
+  //     result = await result.json();
+  //     console.log(result);
+  //     fileDownload(result.data, "downloaded123.png");
+  // }
+
+  async function DownloadImg(e) {
+    let myURL = e.currentTarget.getAttribute("data-value");
+    console.log(myURL);
+
+    axios({
+      url: "http://localhost:5000/download",
+      method: "POST",
+      responseType: "blob",
+      data: JSON.stringify({ myURL }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((resp) => {
+      console.log(resp);
+      fileDownload(resp.data, "downloaded123.png");
+    });
+
+    let result = await fetch("http://localhost:5000/download", {
+      method: "post",
+      responseType: "blob",
+    });
+
+    result = await result.json();
+    console.log(result);
+
+    fileDownload(result.data, "downloaded123.png");
+  }
+
+  return (
+    // <div className='uploaded-potrait'>
+
+    //     {
+    //         data.map((singleData, index) => {
+    //             return (
+
+    //                 <div className="image-3" key={index + 1}>
+    //                     <div className='all-images'>
+
+    //                         {/* {console.log(singleData.name)} */}
+
+    //                     </div>
+    //                     <div className='all-names'>
+    //                         <h1>{singleData.name}</h1>
+    //                         <h2>{singleData.desc}</h2>
+
+    //                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ratione, aspernatur possimus eos fuga a sint
+    //                             velit, esse maxime iste tenetur libero, praesentium officiis quae porro necessitatibus ducimus quibusdam
+    //                             obcaecati ex alias at doloremque eius id! Amet et iure, natus eum nisi porro corrupti asperiores earum
+    //                             voluptates fugiat vero aut iste. Libero odit dolorum eum deserunt deleniti iusto aliquam non qui porro totam
+    //                             animi doloremque quibusdam quae explicabo quam provident maiores quidem, eos autem temporibus ad. Accusamus
+    //                             dignissimos officia illo, voluptas illum laudantium doloremque enim dolor labore quos inventore assumenda ipsa
+    //                             culpa optio repudiandae ad. Officiis aliquam facere velit sed.</p>
+
+    //                     </div>
+
+    //                 </div>
+    //             )
+    //         })
+    //     }
+    // </div>
+    <div>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search Box"
+          className="searchBox"
+          onChange={(e) => searchPotrait(e.target.value)}
+        />
+        <div className="searchicon1">
+          <BiSearch color="white" onClick={searchPotrait} />
+        </div>
+      </div>
+
+      <div className="flex">
+        {data.length > 0
+          ? data.map((singledata, singleIndex) => {
+              return (
+                <div className="card">
+                  <div>
+                    {/* <h3 className='name-h3'>{singledata?.name}</h3> */}
+                    <img
+                      src={`http://localhost:5000/${singledata?.imgUrl}`}
+                      alt="..."
+                      width="150"
+                    />
+                    <div className="btn-div">
+                      <button
+                        className="btn2"
+                        data-value={singledata?.imgUrl}
+                        onClick={DownloadImg}
+                        type="button"
+                        fileDownload
+                      >
+                        Download
+                      </button>
+                    </div>
+                    {/* <h3 className='desc'>{singledata?.desc}</h3> */}
+                  </div>
+                </div>
+              );
+            })
+          : "no Data Found"}
+      </div>
+    </div>
+  );
+};
+
+export default ShowPotraits;
